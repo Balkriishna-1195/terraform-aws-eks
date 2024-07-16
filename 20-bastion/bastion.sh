28a7fc2890d97f0e39114f44196cc27256b1f0c8
Balkriishna-1195 <venatibalakrishna1195@gmail.com>
2024-07-16 21:03:57 +0530

    adding bastion

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -49,10 +49,10 @@ mv kubectl /usr/local/bin/kubectl
 VALIDATE $? "kubectl installation"
 
 # Helm
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-chmod 700 get_helm.sh
-./get_helm.sh
-VALIDATE $? "helm installation"
+# curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+# chmod 700 get_helm.sh
+# ./get_helm.sh
+# VALIDATE $? "helm installation"
 
 dnf install mysql -y
 VALIDATE $? "MySQL installation"
